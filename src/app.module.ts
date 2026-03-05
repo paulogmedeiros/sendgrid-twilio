b243fc57e4aaac44ba98524f4337b974cea70743
paulogmedeiros <paulogsantosm@gmail.com>
2026-03-05 14:05:33 -0300

    feat(sms): create sms

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmailModule } from './email/email.module';
+import { SmsModule } from './sms/sms.module';
 
 @Module({
-  imports: [EmailModule],
+  imports: [EmailModule, SmsModule],
   controllers: [AppController],
   providers: [AppService],
 })
