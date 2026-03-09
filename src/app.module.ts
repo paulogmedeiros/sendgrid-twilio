99d5799883412c6716cc879e9dc676557b8d6c18
MayconRodrigues <maycon.rodrigues@novoseguros.com.br>
2026-03-09 16:48:30 -0300

    testar erros no banco

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,20 +6,38 @@ import { SmsModule } from './sms/sms.module';
 import { ConfigModule, ConfigService} from '@nestjs/config';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [EmailModule, SmsModule, ConfigModule.forRoot({ 
     isGlobal: true,  cache: true,
   }), 
-  ThrottlerModule.forRoot({
+  ThrottlerModule.forRoot({    
       throttlers: [
         {
           ttl: 60000,  // janela de tempo: 60 segundos (em ms)
           limit: 5,
         }  
     ],
-  }) 
-], 
+  }), 
+  TypeOrmModule.forRootAsync({ 
+    inject: [ConfigService],
+    useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('DB_HOST'),
+        port: Number(config.get<number>('DB_PORT')),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_DATABASE'),
+      
+    autoLoadEntities: true,
+    synchronize: config.get<string>('NODE_ENV') !== 'production',
+    logging: true
+
+  })
+  })
+],
+
   controllers: [AppController],
   providers: [
     AppService, 
@@ -27,7 +45,7 @@ import { APP_GUARD } from '@nestjs/core';
     //   provide: APP_GUARD,
     //   useClass: ThrottlerGuard
     // }
-  ],
+  ]
 })
 
 export class AppModule {}
