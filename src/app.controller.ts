05039e14aa27ff1c97ba6eab1a764ca857fd36de
MayconRodrigues <maycon.rodrigues@novoseguros.com.br>
2026-03-09 14:04:01 -0300

    Throtting adicionado,typeORM instalado

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
+@UseGuards(ThrottlerGuard)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,30 @@ import { AppService } from './app.service';
 import { EmailModule } from './email/email.module';
 import { SmsModule } from './sms/sms.module';
 import { ConfigModule, ConfigService} from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [EmailModule, SmsModule, ConfigModule.forRoot({ 
     isGlobal: true,  cache: true,
-  }
-  )],
+  }), 
+  ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,  // janela de tempo: 60 segundos (em ms)
+          limit: 5,
+        }  
+    ],
+  }) 
+], 
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService, 
+    // {
+    //   provide: APP_GUARD,
+    //   useClass: ThrottlerGuard
+    // }
+  ],
 })
+
 export class AppModule {}
